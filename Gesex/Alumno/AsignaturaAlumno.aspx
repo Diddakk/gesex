8c1068a1505f96a41fe343c40f3fc69ce209d604
Diddakk <diegoriverariera@gmail.com>
2016-06-10 20:32:17 +0200

    listado de examenes

--- Gesex/Alumno/AsignaturaAlumno.aspx
+++ Gesex/Alumno/AsignaturaAlumno.aspx
@@ -5,5 +5,11 @@
     <div>        
         <h3><%=asignatura%></h3>
         <a href="Alumno.aspx">Volver</a> 
+        <asp:PlaceHolder runat="server" ID="ErrorMessage" Visible="false">
+            <p class="text-danger">
+                <asp:Literal runat="server" ID="FailureText" />
+            </p>
+        </asp:PlaceHolder>
+        <asp:PlaceHolder runat="server" ID="ExamenesPlaceHolder" Visible="true" />
     </div>
 </asp:Content>
--- Gesex/Alumno/AsignaturaAlumno.aspx.designer.vb
+++ Gesex/Alumno/AsignaturaAlumno.aspx.designer.vb
@@ -12,4 +12,31 @@ Option Explicit On
 
 
 Partial Public Class AsignaturaAlumno
+    
+    '''<summary>
+    '''Control ErrorMessage.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents ErrorMessage As Global.System.Web.UI.WebControls.PlaceHolder
+    
+    '''<summary>
+    '''Control FailureText.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents FailureText As Global.System.Web.UI.WebControls.Literal
+    
+    '''<summary>
+    '''Control ExamenesPlaceHolder.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents ExamenesPlaceHolder As Global.System.Web.UI.WebControls.PlaceHolder
 End Class
